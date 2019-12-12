2c0f42b91d1b28848f3973afff4afced0a1cbc01
mehulmodihb <33020448+mehulmodihb@users.noreply.github.com>
2019-12-12 11:17:00 +0530

    Update DPDConstants.swift

--- DropDown/helpers/DPDConstants.swift
+++ DropDown/helpers/DPDConstants.swift
@@ -32,7 +32,7 @@ internal struct DPDConstant {
 		static let SeparatorColor = UIColor.clear
 		static let CornerRadius: CGFloat = 2
 		static let RowHeight: CGFloat = 44
-		static let HeightPadding: CGFloat = 20
+		static let HeightPadding: CGFloat = 44
 
 		struct Shadow {
 
